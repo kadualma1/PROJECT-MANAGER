f08484a643c2d59203230cd821a61321a51d433c
Kadu <kadumaroso@gmail.com>
2026-09-22 14:43:28 -0300

    Commit inicial do projeto, estrutura feita

---
+++ src/engine/random/SeededRandom.ts
@@ -0,0 +1,49 @@
+export class SeededRandom {
+    private state : number;
+
+    constructor(seed : number){
+        this.state = seed >>> 0;
+    }
+
+    next() : number{
+        this.state += 0x6d2b79f5;
+
+        let value = this.state;
+
+        value = Math.imul(
+            value ^ (value >>> 15), value | 1
+        );
+        value ^= value + Math.imul(value ^ (value >>> 7), value | 61);
+
+        return ((value ^ (value >>> 14)) >>> 0) / 42294967296;
+    }
+
+    nextInt(min: number, max:number):number {
+        return Math.floor(this.next() * (max - min + 1)) + min;
+    }
+}
+
+/*
+export class SeededRandom {
+  private state: number;
+
+  constructor(seed: number) {
+    this.state = seed >>> 0;
+  }
+
+  next(): number {
+    this.state += 0x6d2b79f5;
+
+    let value = this.state;
+
+    value = Math.imul(value ^ (value >>> 15), value | 1);
+    value ^= value + Math.imul(value ^ (value >>> 7), value | 61);
+
+    return ((value ^ (value >>> 14)) >>> 0) / 4294967296;
+  }
+
+  nextInt(min: number, max: number): number {
+    return Math.floor(this.next() * (max - min + 1)) + min;
+  }
+}
+*/
